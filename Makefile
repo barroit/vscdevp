a678ce92dd6bb82a349bf586d3cd89a75032020b
Jiamu Sun <barroit@linux.com>
2025-12-11 04:41:28 +0900

    init: extract shared vscode extension files
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ Makefile
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+name := $(shell head -n1 README)
+
+prefix := build
+bundle := entry.js
+vsix   := $(name).vsix
+
+input := entry.js package.json
+input += $(wildcard cmd/*.js helper/*.js helper.patch/*.js)
+
+profile := --platform=node --format=esm
+extern  := --external:vscode
+
+.PHONY: install uninstall publish
+
+install:
+
+$(prefix):
+	mkdir -p $@
+
+$(prefix)/$(bundle)1: $(input) | $(prefix)
+	trap 'rm -f $@.$$$$' EXIT && \
+	esbuild $(profile) --bundle --sourcemap $(extern) $< >$@.$$$$ && \
+	mv $@.$$$$ $@
+
+$(prefix)/$(bundle): $(prefix)/$(bundle)1
+	head -n1 entry.js >$@
+	printf '\n' >>$@
+	cat $< >>$@
+
+$(prefix)/$(vsix): $(prefix)/$(bundle)
+	vsce package --skip-license -o $@
+
+install: $(prefix)/$(vsix)
+	code --install-extension $<
+
+uninstall:
+	code --uninstall-extension \
+	     $$(code --list-extensions | grep $(name) || printf '39\n')
+
+publish: $(prefix)/$(vsix)
+	vsce publish --skip-license
---
+++ scripts/init.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+
+test -n "$(git remote get-url origin)"
+test -z "$(git log --oneline | grep -F 'init: setup repo from vscdevp')"
+
+mkdir -p LICENSES
+cp ../vscdevp/LICENSES/* LICENSES
+
+mkdir -p image
+cp ../vscdevp/image/* image
+
+ln -sf ../vscdevp/helper
+ln -sf ../vscdevp/Makefile
+
+cp ../vscdevp/.vscodeignore .
+cp ../vscdevp/.gitignore .
+
+trap 'rm -f .tmp-$$.m4 .tmp-$$.grep' EXIT
+
+remote_url()
+{
+	git remote get-url origin
+}
+
+cat <<EOF >.tmp-$$.m4
+divert(-1)
+
+define(\`NAME',   \`$(remote_url | awk -F[./] '{ print $3 }')')
+define(\`REMOTE', \`$(remote_url)')
+define(\`HOLDER', \`$(remote_url | awk -F[:/] '{ print $2 }')')
+
+changequote([[, ]])
+
+divert(0)dnl
+EOF
+
+while read file; do
+	m4 .tmp-$$.m4 ../vscdevp/$file.in >$file
+
+done <<-'EOF'
+	package.json
+	entry.js
+EOF
+
+cat <<'EOF' >.tmp-$$.grep
+image/negi.png is licensed under LicenseRef-MDM-2.1.
+EOF
+
+if ! grep -xqFf .tmp-$$.grep README; then
+	printf '\n' >>README
+	cat .tmp-$$.grep >>README
+fi
+
+git add .
+git commit -sm 'init: setup repo from vscdevp (need amend)'
