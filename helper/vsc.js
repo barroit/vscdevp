d996b29cf83eb04b1b593f0f0f79b287645dfc0b
Jiamu Sun <barroit@linux.com>
2025-12-12 05:55:59 +0900

    helper: add sanitize_ctx()
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ helper/vsc.js
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+export function sanitize_ctx(ctx)
+{
+	return {
+		environ: ctx.environmentVariableCollection,
+		current: ctx.extension,
+
+		binary: {
+			mode: ctx.extensionMode,
+			path: ctx.extensionPath,
+			uri: ctx.extensionUri,
+		},
+
+		datadir: ctx.globalStoragePath,
+		datadir_uri: ctx.globalStorageUri,
+
+		ws_datadir: ctx.storagePath,
+		ws_datadir_uri: ctx.storageUri,
+
+		logdir: ctx.logPath,
+		logdir_uri: ctx.logUri,
+
+		secret: ctx.secrets,
+
+		state: ctx.globalState,
+		ws_state: ctx.workspaceState,
+
+		lm_access: ctx.languageModelAccessInformation,
+		cmds: ctx.subscriptions,
+	}
+}
