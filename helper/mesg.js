a127f82083cb567e9ee428d0bc25b443b1daddeb
Jiamu Sun <barroit@linux.com>
2025-12-31 13:49:36 +0900

    helper: use correct str_cap()
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- helper/mesg.js
+++ helper/mesg.js
@@ -8,7 +8,7 @@ import { vsc_info, vsc_error, vsc_warn } from './vsc.js'
 
 export function die(mesg, detail)
 {
-	const ui_mesg = capitalize(mesg)
+	const ui_mesg = str_cap(mesg)
 	let option
 
 	if (detail)
@@ -18,14 +18,20 @@ export function die(mesg, detail)
 	throw new Error(`fatal: ${mesg}`)
 }
 
+export function error(mesg)
+{
+	mesg = str_cap(mesg)
+	vsc_error(mesg)
+}
+
 export function warn(mesg)
 {
-	mesg = capitalize(mesg)
+	mesg = str_cap(mesg)
 	vsc_warn(mesg)
 }
 
 export function info(mesg)
 {
-	mesg = capitalize(mesg)
+	mesg = str_cap(mesg)
 	vsc_info(mesg)
 }
